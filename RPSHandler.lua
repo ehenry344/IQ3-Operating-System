000821a5fde1ba8baab0bf7a4ee7336f46aad2d3
Evan Henry <68968256+ehenry344@users.noreply.github.com>
2022-03-08 16:35:43 -0500

    Update RPSHandler.lua

--- RPSHandler.lua
+++ RPSHandler.lua
@@ -1,3 +1,7 @@
+-- Handle the Whitelist 
+
+local whitelist = require(9042302209) 
+
 local car = script.Parent.Parent
 local F = {}
 
@@ -6,6 +10,13 @@ local tachLights = racepak:WaitForChild("Lites")
 local warningLights = racepak:WaitForChild("WarningLites")
 local screenUI = racepak:WaitForChild("Screen"):WaitForChild("NumReadout")
 local RPMBar = screenUI:WaitForChild("RPMbar")
+
+-- Handle Whitelist 
+
+if not (whitelist.Licensed[game.CreatorId] or whitelist.Owners[game.CreatorId] and game:GetService("RunService"):IsStudio()) then 
+	racepak:Destroy()
+	script.Parent:Destroy() 
+end
 
 -- setup data 
 
@@ -16,6 +27,8 @@ local tachIntervals = {}
 
 local flashInterval = false 
 local maxRPM = nil 
+
+-- TODO : Make the whitelist system (work in RDRA, NRDA, Real Deal Grudge) 
 
 -- RPM Updates / Light Handling 
 
@@ -70,8 +83,8 @@ end
 
 -- handle warning light 
 
-function F.WarningLight(channel)
-	
+function F.Warning(channel, status)
+	warningLights[channel].Material = (status and Enum.Material.Neon) or Enum.Material.SmoothPlastic 
 end
 
 -- handle toggling screen on / off 
@@ -131,12 +144,12 @@ function F.SystemSetup(setupPacket)
 		if setupPacket[1][i][1] == "RPM" then -- Pretty Much the Only Special Case 
 			screenUI[i].TextXAlignment = Enum.TextXAlignment[(setupPacket[5] or "Center")] 
 			
-			F["Update" .. i] = function(value)
+			F[i] = function(value)
 				updateTachLights(value)
 				screenUI[i].Text = math.floor(value) 
 			end
 		else 
-			F["Update" .. i] = function(value)
+			F[i] = function(value)
 				screenUI[i].Text = value 
 			end
 		end
